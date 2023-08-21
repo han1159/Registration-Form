e25f922416a4ba86d950d7c5a6f7e81fca2acbfd
han1159 <haniyamumtaj09@gmail.com>
2023-08-21 23:19:35 +0530

    day 2

--- quiz/app.js
+++ quiz/app.js
@@ -1,90 +1,73 @@
  const startbtn = document.getElementById('start-btn')
- const nextbtn = document.getElementById('next-btn')
-
+ const nextbtn = document.getElementById('next')
+ const endbtn = document.getElementById('end')
  const questioncontainerelement = document.getElementById('question-container')
  const questionelement = document.getElementById('question')
  const answerbuttonselement = document.getElementById('ans-buttons')
-
- let shuffledquestions,currentquestionindex;
+const score=document.getElementById('score')
+ let shuffledquestions,currentquestionindex,crtans;
  let quizscore=0
-
+ let no=0;
 startbtn.addEventListener('click',startgame)
 nextbtn.addEventListener('click',()=>
 {
     currentquestionindex++
-    setnextquestion()
+    setquestion()
 })
-
 function startgame(){
-    startbtn.classList.add("hide")
-    shuffledquestions=questions.sort(()=>Math.random()-0.5)
-    currentquestionindex=0
+    startbtn.classList.add("hide");
+    shuffledquestions=questions.sort(()=>Math.random()-0.5);
+    currentquestionindex=0;
+    nextbtn.classList.remove('hide');
     questioncontainerelement.classList.remove('hide');
-    setnextquestion()
-    quizscore=0
-}
-
-
-function setnextquestion(){
-    resetstate()
-    showquestion(shuffledquestions[currentquestionindex])
-}
-
-function showquestion(question){
-    questionelement.innerText=question.question
-    question.answers.forEach((answer)=>{
-        const
-    button=document.createElement('button');
-        button.innerText=answer.text;
-        button.classList.add('btn');
-
-        if(answer.correct){
-            button.dataset.correct=answer.correct
-        }
-        button.addEventListener('click',selectans)
-        answerbuttonselement.appendChild(button)
-    })
+    quizscore=0;
+    setquestion()
 
 }
 
-function resetstate(){
+function setquestion(){
+    let i=0
+
+   if(currentquestionindex<questions.length) {questionelement.innerText=questions[currentquestionindex].question
+}
+else{
     nextbtn.classList.add('hide')
-    while(answerbuttonselement.firstChild){
-        answerbuttonselement.removeChild(answerbuttonselement.firstChild)
+    endbtn.classList.remove('hide')
+
+}
+document.getElementById('1').innerText=questions[currentquestionindex].answers[0].text
+document.getElementById('2').innerText=questions[currentquestionindex].answers[1].text
+document.getElementById('3').innerText=questions[currentquestionindex].answers[2].text
+document.getElementById('4').innerText=questions[currentquestionindex].answers[3].text
+}
+function handleClick(button) {
+    const answerButtons = document.querySelectorAll('.btn');
+    answerButtons.forEach(btn => {
+        btn.classList.toggle('selected');
+        btn.classList.remove('selected');
+    });
+    button.classList.add('selected');
+    correctans()
+    if(button.innerText==crtans){
+        ++quizscore
     }
-    
+
 }
 
-function selectans(e){
-    const selectedbtn=e.target
-    const correct=selectedbtn.dataset.correct
-
-    setstatusclass(document.body,correct)
-    
-    Array.from(answerbuttonselement.children).forEach((button)=>{
-        setstatusclass(button,button.dataset.correct)
-    })
-    if(shuffledquestions.length>currentquestionindex+1){
-        nextbtn.classList.remove('hide')
-    }
-    else{
-        startbtn.innerText="restart"
-        startbtn.classList.remove('hide')
-    }
-    if(selectedbtn.dataset==correct){
-        quizscore++
-    }
-    document.getElementById('right-answers').innerText=quizscore
-}
-
-function setstatusclass(element,correct){
-    if(correct){
-        element.classlist.add("correct");}
-    else{
-        element.classlist.add("wrong");
+function correctans(){
+    while(no<4){
+        if(questions[currentquestionindex].answers[no].correct){
+            crtans=questions[currentquestionindex].answers[no].text
+            break
+        }
+        no+=1
     }
 }
 
+function showresult(){
+    score.classList.remove('hide')
+    document.getElementById('right-answers').innerHTML=quizscore
+}
 
 
  const questions=[
